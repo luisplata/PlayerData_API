dabbab41cbbaab955f2a1636bfc1c3f6231905d3
Luis Plata <www.luisplata@gmail.com>
2024-12-12 09:56:33 -0500

    add new database if not exist

--- init.sql
+++ init.sql
@@ -1,1 +1,5 @@
-CREATE DATABASE playerdb;
+CREATE DATABASE IF NOT EXISTS playerdb;
+USE playerdb;
+
+-- Aquí puedes agregar más comandos SQL si lo necesitas
+-- Por ejemplo, crear tablas, insertar datos iniciales, etc.
