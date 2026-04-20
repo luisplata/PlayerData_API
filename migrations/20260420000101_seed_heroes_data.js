e11fb289a87c6bb6b6e89a512c3a460c3ed52b20
Luis Plata <www.luisplata@gmail.com>
2026-04-20 00:22:27 -0500

    agregtando seed en migracion  para poder trankearlos

---
+++ migrations/20260420000101_seed_heroes_data.js
@@ -0,0 +1,24 @@
+exports.up = async function (knex) {
+  const heroId = 'hero-nova';
+  const now = new Date();
+
+  await knex('heroes').where({ heroId }).del();
+
+  await knex('heroes').insert({
+    heroId,
+    name: 'Nova',
+    metadata: JSON.stringify({
+      role: 'support',
+      xpPerLevel: 100,
+      pointsLostPerGame: 2,
+      minPointsGainedPerConversation: 1,
+      pointsGainedPerConversationComplete: 10
+    }),
+    created_at: now,
+    updated_at: now
+  });
+};
+
+exports.down = async function (knex) {
+  await knex('heroes').where({ heroId: 'hero-nova' }).del();
+};
